3f4f40e903d4057d111c2bf666a2d84b0cbfc4b8
Matias Mariani <hi@matiasmariani.io>
2017-01-23 18:24:56 -0500

    general setup and layout

--- app/scripts/components/artistpage.jsx
+++ app/scripts/components/artistpage.jsx
@@ -2,20 +2,68 @@ var React = require('react');
 var ReactDOM = require('react-dom');
 var $ = require('jquery');
 
+
 //############ COMPONENT IMPORTS ###################/
 var NavFooter = require('../templates/nav-footer.jsx').NavFooter;
 
+//############ CONTAINERS #########################/
 
+var HeaderContainer = React.createClass({
+  render: function(){
 
+    return (
+      <div>
 
+        <div id="header" className="row">
+          <div id="header-image" className="col-md-12"/>
+        </div>
 
+        <div  className="row">
+          <div id="artist-content" className="col-md-8 col-md-offset-2 z-depth-2">
+            <div id="artist-title" className="col-md-12">
+              <h3>Eranga Goonetilleke</h3>
+            </div>
+            <div id="about" className="col-md-12">
+              <div id="about-img" className="col-md-5"/>
+              <div id="about-par" className="col-md-7">
+                <p>Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.
+                </p>
+              </div>{/*end about-par*/}
+            </div>{/*end about*/}
+            <div id="about-video" className="row">
+              <Youtube code="B9FiiY2Xkuk"/>
+            </div>
+          </div>
+        </div>{/*end artist-content*/}
+      </div>
+
+    )
+  }
+});
+
+var Youtube = React.createClass({
+    render: function() {
+        return (
+            <div className="videoWrapper col-md-12">
+              <div className="video row">
+                <iframe src={"http://www.youtube.com/embed/" + this.props.code}
+                        width="100%"
+                        height="350px"
+                        frameBorder="0"
+                        allowFullScreen>
+                </iframe>
+                </div>
+            </div>
+        );
+    }
+});
 
 
 var ArtistContainer = React.createClass({
     render: function() {
         return (
           <NavFooter>
-            
+            <HeaderContainer/>
           </NavFooter>
         );
     }
--- app/scripts/templates/nav-footer.jsx
+++ app/scripts/templates/nav-footer.jsx
@@ -12,30 +12,52 @@ var NavFooter = React.createClass({
 
     return (
       <div className="content">
-        <nav className="nav">
-            <div className="nav-wrapper">
-              <a href="#" className="brand-logo">Eranga Goonetilleke</a>
-              <a href="#" data-activates="mobile-demo" className="button-collapse"><i className="material-icons">menu</i></a>
-              <ul id="nav-mobile" className="right hide-on-med-and-down">
-                <li><a href="sass.html">Sass</a></li>
-                <li><a href="badges.html">Components</a></li>
-                <li><a href="collapsible.html">JavaScript</a></li>
-              </ul>
-              <ul className="side-nav" id="mobile-demo">
-                <li><a href="sass.html">Sass</a></li>
-                <li><a href="badges.html">Components</a></li>
-                <li><a href="collapsible.html">JavaScript</a></li>
-              </ul>
+        <div className="navbar-fixed">
+          <nav className="nav">
+              <div className="nav-wrapper">
+                <a href="#" className="brand-logo">Eranga Goonetilleke</a>
+                <a href="#" data-activates="mobile-demo" className="button-collapse"><i className="material-icons">menu</i></a>
+                <ul id="nav-mobile" className="right hide-on-med-and-down">
+                  <li><a href="sass.html">Artist</a></li>
+                  <li><a href="badges.html">Studio</a></li>
+                  <li><a href="collapsible.html">Media</a></li>
+                </ul>
+                <ul className="side-nav" id="mobile-demo">
+                  <li><a href="sass.html">Artist</a></li>
+                  <li><a href="badges.html">Studio</a></li>
+                  <li><a href="collapsible.html">Media</a></li>
+                </ul>
+              </div>
+            </nav>
+          </div>
 
-            </div>
-          </nav>
-
-        <main>
+        <main className="row">
           {this.props.children}
         </main>
 
-        <footer>
-          <div>test footer</div>
+        <footer className="page-footer">
+          <div className="container">
+            <div className="row">
+              <div className="col l6 s12">
+                <h5 className="white-text">Contact me!</h5>
+                <p className="grey-text text-lighten-4">You can use rows and columns here to organize your footer content.</p>
+              </div>
+              <div className="col l4 offset-l2 s12">
+                <h5 className="white-text">Links</h5>
+                <ul>
+                  <li><a className="grey-text text-lighten-3" href="#!">Artist</a></li>
+                  <li><a className="grey-text text-lighten-3" href="#!">Studio</a></li>
+                  <li><a className="grey-text text-lighten-3" href="#!">Media</a></li>
+                  <li><a className="grey-text text-lighten-3" href="#!">Facebook</a></li>
+                </ul>
+              </div>
+            </div>
+          </div>
+          <div className="footer-copyright">
+            <div className="container">
+            © 2014 Eranga Goonetilleke
+            </div>
+          </div>
         </footer>
       </div>
     )
